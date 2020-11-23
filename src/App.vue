6bb9bafe7936effe78d7b6c238ee5e67f3656486
Ant-on-Bear <74732331+Ant-on-Bear@users.noreply.github.com>
2020-11-23 13:32:42 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  created() {
+    this.$store.dispatch('tryAutoLogin');
+  },
+};
+</script>
